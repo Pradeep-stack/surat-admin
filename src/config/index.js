9ea0ff0578ef5e07728cda8c0bc64b5cf05ce29a
Pradeep-React-Node <dev.react@vibgyorweb.com>
2025-06-09 18:47:42 +0530

    bhdj

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
-// export const API_URL = "https://api.indusdigicart.com/api/v1";
-export const API_URL = "http://localhost:4001/api/v1";
+export const API_URL = "https://api.indusdigicart.com/api/v1";
+// export const API_URL = "http://localhost:4001/api/v1";
 export const IMG_URL = "http://localhost:5000";
 export const CommonImage = "https://indusglobal.s3.ap-south-1.amazonaws.com/avatar-3637425_1280.png";
